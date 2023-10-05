65baa850376e6f19858b8be5e5d7b873b753877c
jackeWin <jackericson98@gmail.com>
2023-10-05 10:49:15 -0400

    Added a scheme question

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from System.system import System
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    scheme = input("Choose coarsify scheme: (1) Average Distance, (2) Encapsulate Residues, (3) CG Martini >>>   ")
     root = tk.Tk()
     root.withdraw()
     file = filedialog.askopenfilename()
-    sys = System(file=file)
+    sys = System(file=file, scheme=scheme)
